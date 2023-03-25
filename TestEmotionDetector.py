27e88565e83fbe1551ebbb23bd5cb6652f9138f2
Thejas0 <97007096+Thejas0@users.noreply.github.com>
2023-03-26 01:17:52 +0530

    integrated facial emotion with hand movement and gui

--- TestEmotionDetector.py
+++ TestEmotionDetector.py
@@ -1,100 +1,124 @@
 import cv2
+from matplotlib.figure import Figure
 import numpy as np
 from keras.models import model_from_json
 from cvzone.HandTrackingModule import HandDetector
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 from scipy.ndimage import gaussian_filter
 import matplotlib.cm as cm
-
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import tkinter as tk
 
 detector = HandDetector(detectionCon=0.8, maxHands=2)
 coordLx = []
 coordLy = []
 coordRx = []
 coordRy = []
+count_emotion = {"Angry": 0, "Disgusted": 0, "Happy": 0,
+                 "Neutral": 0, "Sad": 0, "Surprised": 0, "Fearful": 0}
 emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful",
                 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
-# load json and create model
-json_file = open('emotion_model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-emotion_model = model_from_json(loaded_model_json)
 
-# load weights into new model
-emotion_model.load_weights("emotion_model.h5")
-print("Loaded model from disk")
+def capture():
+    global minnX
+    minnX = 1000
+    global maxxX
+    maxxX = -1000
+    global minnY
+    minnY = 1000
+    global maxxY
+    maxxY = -1000
+    # load json and create model
+    json_file = open('emotion_model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    emotion_model = model_from_json(loaded_model_json)
 
-# start the webcam feed
-cap = cv2.VideoCapture(0)
+    # load weights into new model
+    emotion_model.load_weights("emotion_model.h5")
+    print("Loaded model from disk")
 
-# pass here your video path
-# you may download one from here : https://www.pexels.com/video/three-girls-laughing-5273028/
-# cap = cv2.VideoCapture('C:/Users/user/Videos/Captures/production_ID_5198164')
+    # start the webcam feed
+    cap = cv2.VideoCapture(0)
 
-while True:
-    # Find haar cascade to draw bounding box around face
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, (1280, 720))
-    if not ret:
-        break
-    face_detector = cv2.CascadeClassifier(
-        'haarcascade_frontalface_default.xml')
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    while True:
+        # Find haar cascade to draw bounding box around face
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, (800, 720))
+        if not ret:
+            break
+        face_detector = cv2.CascadeClassifier(
+            'haarcascade_frontalface_default.xml')
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # detect faces available on camera
-    num_faces = face_detector.detectMultiScale(
-        gray_frame, scaleFactor=1.3, minNeighbors=5)
+        # detect faces available on camera
+        num_faces = face_detector.detectMultiScale(
+            gray_frame, scaleFactor=1.3, minNeighbors=5)
 
-    # take each face available on the camera and Preprocess it
-    for (x, y, w, h) in num_faces:
-        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
-        roi_gray_frame = gray_frame[y:y + h, x:x + w]
-        cropped_img = np.expand_dims(np.expand_dims(
-            cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+        # take each face available on the camera and Preprocess it
+        for (x, y, w, h) in num_faces:
+            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
+            roi_gray_frame = gray_frame[y:y + h, x:x + w]
+            cropped_img = np.expand_dims(np.expand_dims(
+                cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
 
-        # predict the emotions
-        emotion_prediction = emotion_model.predict(cropped_img)
-        maxindex = int(np.argmax(emotion_prediction))
-        cv2.putText(frame, emotion_dict[maxindex], (x+5, y-20),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-    hands, img = detector.findHands(frame)  # with draw
-    # hands = detector.findHands(img, draw=False) # without draw
-    print(hands)
-    if hands:
-        handType1 = hands[0]["type"]  # Handtype Left or Right
-        if (handType1 == 'Left'):
-            # Hand 1
-            hand1 = hands[0]
-            lmList1 = hand1["lmList"]  # List of 21 Landmark points
-            bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
-            for i in range(0, 1000):
-                coordLx.append(hand1['bbox'][0])
-                coordLy.append(hand1['bbox'][1])
-            centerPoint1 = hand1['center']  # center of the hand cx,cy
-            # print("Left hand")
+            # predict the emotions
+            emotion_prediction = emotion_model.predict(cropped_img)
+            maxindex = int(np.argmax(emotion_prediction))
 
-        # fingers1 = detector.fingersUp(hand1)
+            count_emotion[emotion_dict[maxindex]] += 1
 
-        if (len(hands) == 2 or handType1 == 'Right'):
-            # Hand 2
-            if (len(hands) == 2):
-                hand2 = hands[1]
-            else:
-                hand2 = hands[0]
-            # lmList2 = hand2["lmList"]  # List of 21 Landmark points
-            bbox2 = hand2["bbox"]  # Bounding box info x,y,w,h
-            centerPoint2 = hand2['center']  # center of the hand cx,cy
-            handType2 = hand2["type"]  # Hand Type "Left" or "Right"
-            for i in range(0, 1000):
-                coordRx.append(hand2['bbox'][0])
-                coordRy.append(hand2['bbox'][1])
-            fingers2 = detector.fingersUp(hand2)
+            cv2.putText(frame, emotion_dict[maxindex], (x+5, y-20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        hands, img = detector.findHands(frame)  # with draw
+        # hands = detector.findHands(img, draw=False) # without draw
+        print(hands)
+        if hands:
+            handType1 = hands[0]["type"]  # Handtype Left or Right
+            if (handType1 == 'Left'):
+                # Hand 1
+                hand1 = hands[0]
+                lmList1 = hand1["lmList"]  # List of 21 Landmark points
+                bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
+                for i in range(0, 1000):
+                    coordLx.append(hand1['bbox'][0])
+                    coordLy.append(hand1['bbox'][1])
+                minnX = min(minnX, hand1['bbox'][0])
+                maxxX = max(maxxX, hand1['bbox'][1])
+                centerPoint1 = hand1['center']  # center of the hand cx,cy
+                # print("Left hand")
 
-    cv2.imshow('Emotion Detection', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            # fingers1 = detector.fingersUp(hand1)
 
+            if (len(hands) == 2 or handType1 == 'Right'):
+                # Hand 2
+                if (len(hands) == 2):
+                    hand2 = hands[1]
+                else:
+                    hand2 = hands[0]
+                # lmList2 = hand2["lmList"]  # List of 21 Landmark points
+                bbox2 = hand2["bbox"]  # Bounding box info x,y,w,h
+                centerPoint2 = hand2['center']  # center of the hand cx,cy
+                handType2 = hand2["type"]  # Hand Type "Left" or "Right"
+                for i in range(0, 1000):
+                    coordLx.append(hand2['bbox'][0])
+                    coordLy.append(hand2['bbox'][1])
+                minnY = min(minnY, hand2['bbox'][0])
+                maxxY = max(maxxY, hand2['bbox'][1])
+                fingers2 = detector.fingersUp(hand2)
+
+        cv2.imshow('Emotion Detection', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cap.release()
+            cv2.destroyAllWindows()
+            break
+    MaxX = maxxX
+    MinX = minnX
+    print("msajdnaskndaks   ", MinX, MaxX)
+    print("Min max", minnX, maxxX)
 #################
 
 
@@ -107,36 +131,28 @@ def myplot(x, y, s, bins=1000):
 
 
 def generateFeedback(x, y):
-    minX = 10000
-    maxX = -10000
-    minY = 10000
-    maxY = -10000
-    difX = 0
-    difY = 0
-    for i in (x):
-        minX = min(minX, i)
-        maxX = max(maxX, i)
-    for j in (y):
-        minY = min(minY, i)
-        maxY = max(maxY, j)
+    print("Min and max of x coords is %d ", " %d ", minnX, minnY)
+    print("Min and max of y coords is %d ", " %d ", minnY, maxxY)
 
-    print("Min and max of x coords is %d ", " %d ", minX, maxY)
-    print("Min and max of y coords is %d ", " %d ", minY, maxY)
-
-    difX += maxX-minX
-    difY += maxY-minY
+    difX = maxxX-minnX
+    difY = maxxY-minnY
     text = "The hotter (red color region ) shows that your hand was placed in the region for longer region"
     if (difX > 150 and difY > 100):
-        print("Their was a good movement of hand  ")
+        print("There was a good movement of hand  ")
     else:
         print("Try moving your hand much frequently ")
 
 
-def plotInit(x, y, hand):
+def plotInit(x, y):
+
+    # root = tk.Tk()
+    # root.geometry("500x600")
+    # frameChartsLT = tk.Frame(root)
+    # frameChartsLT.pack()
     fig, axs = plt.subplots(1, 2)
     # plt.title(title)
-    titleScatter = "Scatter plot"+hand
-    titleHeat = "Heat map "+hand
+    titleScatter = "Scatter plot"
+    titleHeat = "Heat map "
     sigmas = [0, 64]
     # ct = 0
     for ax, s in zip(axs.flatten(), sigmas):
@@ -149,36 +165,72 @@ def plotInit(x, y, hand):
             ax.set_title(titleHeat)
             # ax.set_title("Heatmap of Right hand movements")
 
+    # chart1 = FigureCanvasTkAgg(fig, frameChartsLT)
+    # chart1.get_tk_widget().pack()
     #  displaying feedback
     generateFeedback(x, y)
     plt.show()
 
 
-count_emotion = {"Angry": 0, "Disgusted": 0, "Happy": 0,
-                 "Neutral": 0, "Sad": 0, "Surprised": 0, "Fearful": 0}
-
-
 def analyze():
 
-    labels = []
+    labels1 = []
     sizes = []
 
     for x, y in count_emotion.items():
-        labels.append(x)
+        labels1.append(x)
         sizes.append(y)
 
-    # Plot
-    plt.pie(sizes, labels=labels)
+    root = tk.Tk()
+    root.geometry("500x600")
+    explode = [0.03, 0, 0.1, 0, 0, 0, 0]
+    frameChartsLT = tk.Frame(root)
+    frameChartsLT.pack()
+    fig = Figure(figsize=(4, 2), dpi=100)  # create a figure object
+    ax = fig.add_subplot(111)  # add an Axes to the figure
+    textprops = {"fontsize": 8}
+    ax.pie(sizes, radius=1, labels=labels1, shadow=True,
+           autopct="%0.2f%%", explode=explode, textprops=textprops)
 
-    plt.axis('equal')
-    plt.show()
-    plt.bar(list(count_emotion.keys()), count_emotion.values(), color='g')
-    plt.show()
+    chart1 = FigureCanvasTkAgg(fig, frameChartsLT)
+    chart1.get_tk_widget().pack()
+
+    ###
+    f = Figure(figsize=(4, 2), dpi=100)  # Graph
+    ax = f.add_subplot(111)
+    ll = tk.Label(root, text="No of times the expression was displayed")
+    ll.config(font=("Courier", 14))
+    ll.pack()
+
+    # the x locations for the groups
+    width = 1
+
+    rects1 = ax.bar(list(count_emotion.keys()), count_emotion.values(), width)
+
+    canvas = FigureCanvasTkAgg(f, master=root)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    T = tk.Text(root, height=5, width=52)
+
+    max = 0
+    expr = ""
+    for x, y in count_emotion.items():
+        if (y > max):
+            max = y
+            expr = x
+
+# Create label
+    l = tk.Label(root, text="Analysis ")
+    l.config(font=("Courier", 14))
+    Fact = expr
+    l.pack()
+    T.pack()
+    T.insert(tk.END, Fact)
 #################
 
 
-cap.release()
-cv2.destroyAllWindows()
-plotInit(coordLx, coordLy, "Left hand")
-plotInit(coordRx, coordRy, "Right Hand")
+# capture()
+
+# plotInit(coordLx, coordLy, "Left hand")
+# plotInit(coordRx, coordRy, "Right Hand")
 # analyze()
---
+++ gui.py
@@ -0,0 +1,156 @@
+import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
+from functools import partial
+
+# Import the PhotoImage class from the tkinter module
+from tkinter import PhotoImage, Toplevel
+import os
+import TestEmotionDetector as te
+import time
+# import TestEmotionDetector as TE
+# Create the main window
+
+master = Tk()
+master.title("EX-Press")
+master.geometry("600x455")
+# window = tk.Tk()
+# window.title("EX-Press")
+# window.geometry("600x455")
+# window.config(bg="white")
+
+# image = PhotoImage(file="GUI/LOGO.png")  # Load the image file
+# Resize the image while preserving the aspect ratio
+# image = image.subsample(1, 1)
+# label = tk.Label(image=image)  # Create an image label widget
+# Pack the image label widget to the left of the parent widget
+# label.pack(side="left")
+
+# Create a label widget to display the time
+time_label = tk.Label(master, text="", font=("Arial", 18, "bold"))
+time_label.pack()
+
+
+# Create a button widget
+# button = tk.Button(window, image=image, bg="white")
+
+# Update the time label every second
+
+
+def update_time():
+    # Get the current time in hh:mm:ss AM/PM format
+    current_time = time.strftime("%I:%M:%S %p")
+    time_label.configure(text=current_time)
+    # Run this function again after 1000 milliseconds (1 second)
+    master.after(1000, update_time)
+
+
+update_time()  # Initialize the time label
+
+# Define a function for each button that runs the corresponding Python file
+
+
+def run_file_1():
+    # exec(open('Basic/actions.py').read())
+    print("hii")
+
+
+def run_file_2():
+
+    te.plotInit(te.coordLx, te.coordLy)
+    # te.plotInit(te.coordRx, te.coordRy, "Right Hand")
+    # window1.mainloop()
+
+
+def run_file_3():
+
+    te.analyze()
+    print("hii")
+
+
+def run_file_4():
+    # exec(open('Version 1.1/app.py').read())
+    print("hii")
+
+
+def run_file_5():
+    # exec(open('User Guide/GIT ReadMe.py').read())
+    root = Tk()
+
+    # specify size of window.
+    root.geometry("250x170")
+
+    # Create text widget and specify size.
+    T = Text(root, height=5, width=52)
+
+    # Create label
+    l = Label(root, text="Fact of the Day")
+    l.config(font=("Courier", 14))
+
+    Fact = """A man can be arrested in
+    Italy for wearing a skirt in public."""
+
+    # Create button for next text.
+    b1 = Button(root, text="Next", )
+
+    # Create an Exit button.
+    b2 = Button(root, text="Exit",
+                command=root.destroy)
+
+    l.pack()
+    T.pack()
+    b1.pack()
+    b2.pack()
+
+    # Insert The Fact.
+    T.insert(tk.END, Fact)
+
+    tk.mainloop()
+# Create the buttons
+
+
+button1 = tk.Button(text="Capture", bg="beige",
+                    activebackground="light blue", command=te.capture)
+button1.configure(height=5, width=20)
+button1.pack(pady=5, padx=5)
+button1.config(font=("Playfair", 8, "bold"))
+button1.config(borderwidth=2, relief="groove")
+
+button2 = tk.Button(text="Hand Gesture Analysis", bg="beige",
+                    activebackground="light blue", command=run_file_2)
+# partial(te.plotInit, te.coordLx, te.coordLy, "Left Hand")
+button2.configure(height=5, width=20)
+button2.pack(pady=5, padx=5)
+button2.config(font=("Playfair", 8, "bold"))
+button2.config(borderwidth=2, relief="groove")
+
+button5 = tk.Button(text="Hand Gesture Suggestion", bg="beige",
+                    activebackground="light blue", command=run_file_5)
+button5.configure(height=5, width=20)
+button5.pack(pady=5, padx=5)
+button5.config(font=("Playfair", 8, "bold"))
+button5.config(borderwidth=2, relief="groove")
+
+button3 = tk.Button(text="Facial Expression Analysis", bg="beige",
+                    activebackground="light blue", command=run_file_3)
+button3.configure(height=5, width=20)
+button3.pack(pady=5, padx=5)
+button3.config(font=("Playfair", 8, "bold"))
+button3.config(borderwidth=2, relief="groove")
+
+button4 = tk.Button(text="Speech Analysis", bg="beige",
+                    activebackground="light blue", command=run_file_4)
+button4.configure(height=5, width=20)
+button4.pack(pady=5, padx=5)
+button4.config(font=("Playfair", 8, "bold"))
+button4.config(borderwidth=2, relief="groove")
+
+
+# Pack the buttons into the window
+# button1.pack()
+# button2.pack()
+# button3.pack()
+# button4.pack()
+# Run the main loop
+# window.mainloop()
+mainloop()
